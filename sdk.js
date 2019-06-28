14a5602b9fdc596aba56b44a6e2d441f1cb0b80e
Brian <wl00387719@gmail.com>
2019-06-28 23:56:29 +0800

    first apporch

--- sdk.js
+++ sdk.js
@@ -1,7 +1,31 @@
 
 const SDK = adApi => {
-  const res = fetch(adApi).then(res => JSON.parse(res)).catch(e => console.log(e))
-  console.log('SDK get response', res)
+  
+  function getAd (api) {
+    try {
+      var xhttp = new XMLHttpRequest()
+  
+      xhttp.onreadystatechange = () => {
+        if (this.readyState === 4 && this.status === 200) {
+          this.responseText
+        }
+      }
+
+      xhttp.open('GET', api, true)
+      xhttp.send()
+    } catch (e) {
+
+    }
+  }
+
+
+  function checkResponse (response) {
+    
+  }
+  function generateDomNode (adData) {
+    document.createElement('iframe')
+  }
+  
 }
 
 window.SDK = SDK
